f0cc293f8b122e8f6c5dde77463bcf42feb69dc2
arsad <isaarsad0@gmail.com>
2026-09-09 02:29:19 +0700

    feat(docs): add swagger

---
+++ src/setup-swagger.ts
@@ -0,0 +1,29 @@
+import { type INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('HR Management & Auth API')
+    .setDescription('Official Core API Documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        description: 'Enter your JWT access token',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+}
